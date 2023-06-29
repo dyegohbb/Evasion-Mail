3a41c5e27ad0f264f9343bbfcf38e2272e298349
Dyego H <dyegohbb@gmail.com>
2023-06-28 23:02:20 -0300

    primeiro commit

---
+++ src/main/java/br/com/evasion/mail/EvasionMailApplication.java
@@ -0,0 +1,13 @@
+package br.com.evasion.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvasionMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvasionMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/evasion/mail/EvasionMailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evasion.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvasionMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
